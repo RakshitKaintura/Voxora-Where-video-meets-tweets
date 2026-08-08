08401dde9c830ab8600b9e627b863462da44d46c
RakshitKaintura <blank1951k@gmail.com>
2026-08-08 12:55:33 +0530

    user update fix'

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -248,8 +248,8 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
         throw new ApiError(400, "All fields are required");
     }
 
-    const user = User.findByIdAndUpdate(
-        req.user?.id,
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
         {
             $set: {
                 fullName: fullName,
